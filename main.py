bf253e7ca41d4cd99d658efe57461bb074bd4293
ArturProgrammer-cryptoRespect <Artur.12.krai@gmail.com>
2025-06-03 14:31:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+N=8765378647868767
+summ=0
+for i in range(1,N+1):
+    print(i)
---
+++ rrrr.py
@@ -0,0 +1,9 @@
+for i in range(1, 1001):
+    if i == 1:
+        print(i,"манул")
+    elif i >= 2 and i<= 4:
+        print(i,"манула")
+    else:
+        print(i, "манулов")
+for i in range %2:
+        print (i, "манула")
